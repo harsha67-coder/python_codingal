91b7fb960b2ac9e3b2e2d49884c7abb94b39b472
harsha67-coder <deviniranjharsha@gmail.com>
2025-11-27 20:31:02 +0530

    activityvariableproject

---
+++ activityidk.py
@@ -0,0 +1,18 @@
+print("WELCOME TO RAJ'S BIRHDAY LIST")
+
+jam_bday="15/2/2024"
+harsha_bday="14/2/2022"
+jeeva_bday="12/2/2004"
+kavin_bday="31/2/2006"
+sai_bday="4/2/2025"
+#this program is to show the various birthdays of raj's friends
+print("1.james b'day")
+print(jam_bday)
+print("2.harsha b'day")
+print(harsha_bday)
+print("3.jeeva b'day'")
+print(jeeva_bday)
+print("4.kavin b'day")
+print(kavin_bday)
+print("5.sai b'day'")
+print(sai_bday)
