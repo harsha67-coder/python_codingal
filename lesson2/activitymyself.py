598d4e63a415833314219a8f21edb3df58c87651
harsha67-coder <deviniranjharsha@gmail.com>
2025-12-03 11:04:36 +0530

    activitydatatypes

---
+++ lesson2/activitymyself.py
@@ -0,0 +1,18 @@
+print("good morning user ")
+ans=input("type ur answer here :")
+print("good morning\n")
+print(ans)
+num= int(input("type a number here :"))#only 1 digit allowed
+for i in range(1,11):
+    print(num+"*"+i+"="+(num*i))
+""" print(user + "*2")
+print(user * 2)
+print(user* 3)
+print(user * 4)
+print(user * 5)
+print(user * 6)
+print(user * 7)
+print(user * 8)
+print(user * 9)
+print(user * 10) """
+#
---
+++ lesson3/activity1.py
@@ -0,0 +1,8 @@
+a=56
+print("type of a ",a,"is",type(a))
+b="hello"
+print("type of b  ",b,"is",type(b))
+c=5.6
+print("type of b ",c,"is",type(c))
+d=True
+print("type of b  ",d,"is",type(d))
