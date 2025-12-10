17d9fd2e71fb95a6ff9d736bb8db58f6d769bdde
harsha67-coder <deviniranjharsha@gmail.com>
2025-12-10 17:31:59 +0530

    afterclassproject

---
+++ lesson5/afterclassproject.py
@@ -0,0 +1,8 @@
+print("WELCOME TO THE BEST CLOTHING OPTIONS FOR THE RIGHT TEMPERATURE")
+climate_input=float(input("enter the temp outside here :"))
+if climate_input >=40:
+    print("the temp outside is very hot,so the best clothing would be a cotton t-shirt")
+elif climate_input<=15:
+    print("the temp  outside is very low so wearing a thick tshirt and a hoodie on top would be perfect ")
+elif climate_input <=5:
+     print("the temp  outside is very low so wearing a thick tshirt ,a hoodie and a jacket on top would be perfect ")
