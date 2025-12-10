96d45ec7fb764c462549808550a16df91e28ef38
harsha67-coder <deviniranjharsha@gmail.com>
2025-12-10 17:06:46 +0530

    activity3goat

---
+++ lesson5/activity3.py
@@ -0,0 +1,9 @@
+user1=int(input("enter the first number here :"))
+user2=int(input("enter the second number here :"))
+
+if user1 == user2:
+    print("the numbers are both the same!!")
+elif user1 > user2:
+    print(user1,"is greater than",user2)
+else :
+    print(user2,"is greater than",user1)
