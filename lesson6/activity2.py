925c58422c98637b025d8dac1ef57035e4670032
harsha67-coder <deviniranjharsha@gmail.com>
2025-12-13 10:58:56 +0530

    activitynotequal

---
+++ lesson6/activity2.py
@@ -0,0 +1,6 @@
+x= int(input("enter a number here :"))
+y=int(input("enter a number here :"))
+if x!=y:
+    print("the numbers u have typed are not the same!")
+else:
+    print("the numbers u have typed are the same")
