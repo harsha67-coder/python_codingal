31d35691470531026ad23464ab41f5f71df3490c
harsha67-coder <deviniranjharsha@gmail.com>
2025-12-13 10:51:39 +0530

    activityboolean

---
+++ lesson6/activity1.py
@@ -0,0 +1,10 @@
+a=4
+b=5
+c=2
+d=0
+if a and b and c and d:
+    print("all the numbers have the boolean as true")
+elif a or b or c or d:
+    print("atleast one number has boolean as false")
+else:
+    print("invalid number")
