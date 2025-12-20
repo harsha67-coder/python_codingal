f6d7b642f49ddaa6a64f584d7000dc684cd18a79
harsha67-coder <deviniranjharsha@gmail.com>
2025-12-20 17:24:09 +0530

    activity2fordaday

---
+++ lesson8.py/activity2.py
@@ -0,0 +1,6 @@
+num1=int(input("enter the number here:"))
+num2=int(input("enter the number here:"))
+if num1%num2==0:
+    print("the numbers are divisible by each other")
+else:
+    print("not divisible by each other")
