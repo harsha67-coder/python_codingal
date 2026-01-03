f299935bc0005598047481aea49dba38bc2aa755
harsha67-coder <deviniranjharsha@gmail.com>
2026-01-03 12:15:05 +0530

    activity10

---
+++ afterclassproject9.py/activity1.py
@@ -0,0 +1,4 @@
+user_age=int(input("pls enter ur age here:"))
+if user_age>10:
+    print("u r under 10")
+elif user_age
---
+++ lesson10.py/activity1.py
@@ -0,0 +1,5 @@
+user_input=int(input("enter a number here:"))
+sum=0
+for i in range(0,user_input+1):
+    sum=sum=i
+print(sum)
