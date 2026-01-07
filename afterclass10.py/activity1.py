12c1474ef5620560e0046c68130256958f32af07
harsha67-coder <deviniranjharsha@gmail.com>
2026-01-07 17:37:54 +0530

    activityafterclas10pt.2

--- afterclass10.py/activity1.py
+++ afterclass10.py/activity1.py
@@ -1,4 +1,4 @@
 x=int(input("enter a number here for power to be calculated:"))
 n=int(input("enter the power :"))
 ans=x**n
-print(ans)
+print(" Exponential of "+str(2)+" power " + str(n)+" is "+str(ans))
