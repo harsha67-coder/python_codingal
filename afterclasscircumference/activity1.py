38954399371fd2b618c6c249c6cac185cd98dfa0
harsha67-coder <deviniranjharsha@gmail.com>
2026-01-24 18:32:18 +0530

    activitycircle

---
+++ afterclasscircumference/activity1.py
@@ -0,0 +1,4 @@
+radius=int(input("enter the radius to find the circumference of circle :"))
+print("radius is",radius)
+circumference=2*(22/7)*radius
+print("the circumference of the circle is:",circumference)
