b547c1d61882bf2d7515ebc681a61fbf0d2b8e69
harsha67-coder <deviniranjharsha@gmail.com>
2026-02-14 18:03:37 +0530

    activityrandom,timemodule

---
+++ calenderDateTimemodule.py/activitytimeandrandom.py
@@ -0,0 +1,16 @@
+import random
+import time
+
+def getrandomdate(start_time,end_time):
+    print("an random date between",start_time,"and",end_time,"is:")
+    randomdate=random.random()
+    dateformat='d%/m%/y%'
+    start_time = time.mktime(time.strptime(end_time,start_time))
+    end_time = time.mktime(time.strpttime(start_time,end_time))
+    randomtime = start_time+randomdate*(start_time-end_time)
+    randomdaydate=time.strfttime(dateformat,time.localtime(randomtime)) 
+    return randomdaydate
+print("randomdate=",getrandomdate,("1/1/2027")("1/1/2028"))
+
+
+    
