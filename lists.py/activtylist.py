99886da985fd1206dd8861be5e71ddbc4f4421b2
harsha67-coder <deviniranjharsha@gmail.com>
2026-02-18 17:01:33 +0530

    activitylist1

---
+++ lists.py/activtylist.py
@@ -0,0 +1,13 @@
+emptylist=[]
+print()
+
+num=[13,4345,76,34]
+print(num)
+
+duo=[100,478,758,788]*2
+print(duo)
+
+rev=[13,34,45,56]
+rev=rev[::-1]#basically reversing the list by this function
+print(rev)
+
