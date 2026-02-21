48db3f7c1bbfc95eb9e1937a1c336dadc8fcaf77
harsha67-coder <deviniranjharsha@gmail.com>
2026-02-21 17:13:58 +0530

    activitytuple1

---
+++ tuples.py/activitytuple.py
@@ -0,0 +1,15 @@
+tuple=(123,"gg",456,"hi")
+print(tuple)
+
+tuple=(3,4,5,6,7)
+print(tuple)
+
+tuple=tuple+(8,)#must add comma as then only it will be treated as tuple to be added
+print(tuple)
+
+tuple1=(400,400,500,600,700,700)
+print(tuple1.count(400))
+
+tuplex=(41,78,90,9,65,0)
+slice=tuplex[3:5]
+print(slice)
