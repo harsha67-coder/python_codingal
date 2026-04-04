0ceab4b12b0f747ac6e02f8507dd4813cfbbdd2c
harsha67-coder <deviniranjharsha@gmail.com>
2026-04-04 17:14:03 +0530

    activityclass,objectno.1

---
+++ activityclass,object.py/activity1.py
@@ -0,0 +1,6 @@
+class student:
+    grade=9
+    print("hi am a student of grade",grade)
+
+student1=student()
+student2=student()
---
+++ advancelists.py/activity_lambda.py
@@ -0,0 +1,13 @@
+numbers1=[1,2,3,4]
+numbers2=[5,6,7,8]
+result=list(map(lambda x,y:x+y,numbers1,numbers2))
+print("sum of the 2 are:",result)
+
+nums=[1,2,3,4,5,6,7,8,9]
+
+square= list(map(lambda x:x*x,nums))
+print("squares of the nums are:",square)
+
+
+
+    
